27191d7aa4a77f9ab6b3ead8904f4b57a72f74f9
Pedro <pedro.dpaula67@gmail.com>
2023-11-23 16:55:59 -0300

    Update noticia-insere.php

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -16,6 +16,12 @@ if(isset($_POST['inserir'])){
 
 	/* Enviando o arquivo para o servidor */
 	upload($imagem);
+
+	/* Chamado a função para inserir a notícia */
+	inserirNoticia($conexao, $titulo, $texto, $resumo, $imagem['name'], $usuarioId);
+
+	header("location:noticias.php");
+
 }
 ?>
 
