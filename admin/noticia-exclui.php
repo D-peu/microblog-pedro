f8351729d70c8dc0ae4b00e13a115f5d4e2bb1b8
Pedro <pedro.dpaula67@gmail.com>
2023-11-29 15:19:54 -0300

    poder excluir noticia

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -1,3 +1,14 @@
 <?php
-require "funcoes-sessao.php";
+require "../inc/funcoes-sessao.php";
+require "../inc/funcoes-noticias.php";
 verificaAcesso();
+
+verificaTipo();
+
+
+$idNoticia = $_GET['id'];
+$idUsuario = $_SESSION['id'];
+$tipoUsuario = $_SESSION['tipo'];
+
+excluirNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario);
+header("location:noticias.php");
